f00ee123e096f19e9b5b0ec0109917ba234c80df
Staffan Frisk <info@evol.se>
2016-03-31 14:53:37 +0200

    With Web API

---
+++ src/test/java/com/example/DasBootApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import com.boot.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@WebAppConfiguration
+public class DasBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
